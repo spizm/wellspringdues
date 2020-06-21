a90f9ec77c069aa214e0740f2147898230352db7
Michael Spiz <mike@spiz.us>
2020-06-20 18:59:14 -0700

    initial commit

---
+++ js/dues.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var stripe = Stripe('pk_test_8mGh7tU4ba7EdfegNCiIzZ3f00nGkiJB4b');
+  var checkoutButton = document.getElementById('dues-button');
+
+  checkoutButton.addEventListener('click', function () {
+    stripe.redirectToCheckout({
+        lineItems: [{
+          price: 'price_1GwBrnDrsB0XpAimjzFQsNTP',
+          quantity: 1
+        }],
+        mode: 'payment',
+        successUrl: 'https://www.wellspringensemble.org/success',
+        cancelUrl: 'https://www.wellspringensemble.org/canceled',
+      })
+      .then(function (result) {
+        if (result.error) {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer.
+          var displayError = document.getElementById('error-message');
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
+});
