ca9bd9b50b48024eed95798aab335d2433c5f672
Michael Spiz <mike@spiz.us>
2020-06-20 21:57:10 -0700

    add optional donation and go live

--- js/dues.js
+++ js/dues.js
@@ -1,16 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var stripe = Stripe('pk_test_8mGh7tU4ba7EdfegNCiIzZ3f00nGkiJB4b');
+  var stripe = Stripe('pk_live_6m9utrX4rqny496YVGmwQnYP00eXfwPMQN');
   var checkoutButton = document.getElementById('dues-button');
 
   checkoutButton.addEventListener('click', function () {
+    var donationPrice = document.querySelector('input[name="donation-amount"]:checked').dataset.price;
+    var duesPrice = 'price_1GvFq1DrsB0XpAimAHKzRNZR';
+
+    var items = [{
+      price: duesPrice,
+      quantity: 1
+    }]
+
+    if (donationPrice) {
+      items.push({
+        price: donationPrice,
+        quantity: 1
+      })
+    }
+
     stripe.redirectToCheckout({
-        lineItems: [{
-          price: 'price_1GwBrnDrsB0XpAimjzFQsNTP',
-          quantity: 1
-        }],
+        lineItems: items,
         mode: 'payment',
         successUrl: 'https://dues.wellspringensemble.org/success',
-        cancelUrl: 'https://dues.wellspringensemble.org/canceled',
+        cancelUrl: 'https://dues.wellspringensemble.org/',
+        billingAddressCollection: 'required',
       })
       .then(function (result) {
         if (result.error) {
